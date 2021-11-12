5a3f4a156879fcb14e65e2bcf82cc79ff09f6b10
Jeeson <dominguez_js@outlook.com>
2021-11-11 21:45:46 -0600

    Conducting router and getting item correct info

--- WiredBrainCoffee/Services/MenuService.cs
+++ WiredBrainCoffee/Services/MenuService.cs
@@ -14,13 +14,15 @@ namespace WiredBrainCoffee.Services
             {
                 new MenuItem()
                 {
+                    Id= 0,
                     Name = "Frosted Pumpkin Bread",
                     Summary = "A seasonal delight we offer every autumn.  Pumpking bread with just a bit of spice, cream cheese frosting with just a hint of home.",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
-                    ImageFile = "pumpkinbread.png"
+                    ImageFile = "pumpkinbread.png",
                 },
                 new MenuItem()
                 {
+                    Id= 1,
                     Name = "Ground to Go",
                     Summary = "Love our coffee? Take it with you so you never have to be without!",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
@@ -28,6 +30,7 @@ namespace WiredBrainCoffee.Services
                 },
                 new MenuItem()
                 {
+                    Id= 2,
                     Name = "Granola with Nuts",
                     Summary = "It's not flashy, but it sure is healthy.  Perfect for when you need the calories, but not the guilt.",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
@@ -35,6 +38,7 @@ namespace WiredBrainCoffee.Services
                 },
                 new MenuItem()
                 {
+                    Id= 3,
                     Name = "Bean there, done that!",
                     Summary = "Do you prefer to grind your own coffee? No problem, we'll give you the beans.",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
@@ -49,6 +53,7 @@ namespace WiredBrainCoffee.Services
                 },
                 new MenuItem()
                 {
+                    Id= 0,
                     Name = "Fresh Fruit",
                     Summary = "We've got strawberries, blueberries, apples, bananas - we could list them all, but we'd prefer you come take a look!",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
